865e5342b2e4885f9e7ccffb17f0f35942895fec
GRACEFULWIND\Administrator <Gracefulwindbigwang@gmail.com>
2018-07-18 11:20:22 +0800

    no message

---
+++ app/src/main/java/com/wind/testlib/TestMethod1.java
@@ -0,0 +1,14 @@
+package com.wind.testlib;
+
+/**
+ * Created by Gracefulwind Wang on 2018/7/18.
+ * Email : Gracefulwindbigwang@gmail.com
+ *
+ * @author : Gracefulwind
+ */
+public class TestMethod1 {
+    public static void print(String str){
+
+        System.out.println("test print ::" + str);
+    }
+}
